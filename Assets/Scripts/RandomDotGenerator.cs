5bf0fbd4c7ff970b6ef2e2d29d4d53cefed17c9f
Kaz <onishi.kazuho@lab.ime.cmc.osaka-u.ac.jp>
2024-06-19 23:03:09 +0900

    ランダムにドットを動かす機能を追加

--- Assets/Scripts/RandomDotGenerator.cs
+++ Assets/Scripts/RandomDotGenerator.cs
@@ -1,17 +1,19 @@
-﻿using UnityEngine;
+﻿using Meta.WitAi;
+using UnityEngine;
 
 public class RandomDotGenerator : MonoBehaviour
 {
     public GameObject viewCamera; // ドットを向けるカメラ
     public int seed = 1; // ランダム生成のためのシード値
     public float RandomDotsDistance = 10; // ドットの距離
-    [Range(0, 180)]
+    [Range(1f, 180)]
     public float RandomDotsAngle = 90; // ドットが配置される範囲の角度
     public float RandomDotsDensity = 10; // ドットの密度
     public float RandomDotsSize = 0.1f; // ドットの基本サイズ
     public float RandomDotsMaxHeight = 20; //ランダムドットの生成範囲（左右に振る場合の上下）の指定
     public int RandomDotsAmount; // 生成されるドットの数
 
+    private GameObject parent;
     void Awake()
     {
         //カメラ割り当ててないなら探してみる
@@ -29,35 +31,51 @@ public class RandomDotGenerator : MonoBehaviour
         }
     }
 
-    public void CreateRandomDot(bool adjustScale = true)
+    /// <summary>
+    /// ランダムドットを生成
+    /// </summary>
+    /// <param name="adjustScale">ランダムドットのサイズが遠いものほど大きくなるようにするか</param>
+    /// <returns>ランダムドットの親オブジェクト</returns>
+    public GameObject CreateRandomDot(bool adjustScale = true)
     {
+        if (parent != null) Destroy(parent);
         Random.InitState(seed); // ランダムジェネレータの初期化
         float areaSize = 2 * Mathf.PI * RandomDotsDistance * RandomDotsMaxHeight * RandomDotsAngle / 180f; // ドットが配置される領域（面積）の計算
         int numberOfDots = (int)(areaSize * RandomDotsDensity); // ドットの数の計算
         RandomDotsAmount = numberOfDots;
         float baseSize = RandomDotsSize;
-        GameObject parent = new GameObject("RandomDot"); // ドットの親オブジェクトを作成
+        parent = new GameObject("RandomDot"); // ドットの親オブジェクトを作成
         parent.tag = "Dots"; // タグを設定
         parent.transform.position = viewCamera.transform.position;
 
         for (int i = 0; i < numberOfDots; i++)
         {
-            float theta = Random.Range(90 - RandomDotsAngle, 90 + RandomDotsAngle) / 180 * Mathf.PI; // ドットのカメラからの方向を決定
-            float x = RandomDotsDistance * Mathf.Cos(theta); // x位置を求める
-            float y = Random.Range(-RandomDotsMaxHeight, RandomDotsMaxHeight); // y位置をランダムに設定
-            float z = RandomDotsDistance * Mathf.Sin(theta); // z位置を求める
-            Vector3 randomPosition = parent.transform.position + new Vector3(x, y, z);
+            // 球体ドットを作成
+            GameObject dot = GameObject.CreatePrimitive(PrimitiveType.Sphere); 
+            Renderer dotRenderer = dot.GetComponent<Renderer>();
+            dotRenderer.material = new Material(Shader.Find("Unlit/Color")); // マテリアル設定
+            dotRenderer.material.color = Color.white;
 
-            GameObject dot = GameObject.CreatePrimitive(PrimitiveType.Sphere); // 球体ドットを作成
+            // 球体の高度調整
+            float y = Random.Range(-RandomDotsMaxHeight, RandomDotsMaxHeight); // y位置をランダムに設定
+            Vector3 randomPosition = parent.transform.position + new Vector3(0, y, RandomDotsDistance);
             dot.transform.position = randomPosition;
+
+            //球体の方向調整
+            var centerObject = new GameObject("Center");
+            centerObject.transform.SetParent(parent.transform);
+            centerObject.transform.localPosition = Vector3.zero;
+            dot.transform.SetParent(centerObject.transform); // 親オブジェクトに設定
+            float theta = Random.Range(-RandomDotsAngle, RandomDotsAngle);
+            centerObject.transform.localEulerAngles = new Vector3(0, theta, 0);
+
+            // 球体のサイズ調整
             float distance = RandomDotsDistance;
             if (adjustScale) distance = Vector3.Distance(randomPosition, viewCamera.transform.position); // カメラからの距離に基づいたサイズ調整
             float adjustedSize = baseSize * distance / RandomDotsDistance;
             dot.transform.localScale = new Vector3(adjustedSize, adjustedSize, adjustedSize);
-            Renderer dotRenderer = dot.GetComponent<Renderer>();
-            dotRenderer.material = new Material(Shader.Find("Unlit/Color")); // マテリアル設定
-            dotRenderer.material.color = Color.white;
-            dot.transform.SetParent(parent.transform); // 親オブジェクトに設定
+
         }
+        return parent;
     }
 }
--- Assets/Scripts/SwingMotion.cs
+++ Assets/Scripts/SwingMotion.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class SwingMotion : MonoBehaviour
 {
@@ -9,17 +11,35 @@ public class SwingMotion : MonoBehaviour
         FORWARDBACK = 2
     }
 
-    public SWING_TYPE type; //スイングする方向を指定
-    public bool isSwinging = false; // スイングがアクティブかどうかのフラグ
+    public enum MOVE_TYPE
+    {
+        SWING = 0,
+        RANDOM = 1
+    }
+
+    public SWING_TYPE direction; //スイングする方向を指定
+    public MOVE_TYPE moveType; //スイングするかランダムに動かすか指定
+    public bool isMoving = false; // スイングがアクティブかどうかのフラグ
     public float swingAngle = 45.0f; // スイングの角度(SIDE,UPDOWN用)
     public float swingRange = 5f; // 前後移動の範囲(FORWARDBACK用)
     public float frequency = 0.2f; // スイングの頻度（秒間何往復するか）
     public RandomDotGenerator generator; //RandomDot生成用
 
+    [Header("ランダムに動かす際のパラメータ")]
+    [Tooltip("ドットをランダムに動かす際に方向を変える頻度")]
+    public int changeDirectionFrequency = 10;
+    [Tooltip("最大で秒間にどれだけ動くか")]
+    public float translateRange = 1;
+    [Tooltip("最大で秒間にどれだけ回るか（度）")]
+    public float rotateRange = 10;
+
     private float angle = 0f; // 現在の角度
     private GameObject dot; // スイングするドットオブジェクト
-    private float passedTime; //スイング開始時にドットの位置が急に変わらないようにするために経過時間で回転
-    private Vector3 defaultPosition; //前後移動用に保存しておくドットの基準位置
+    private float passedTime; // スイング開始時にドットの位置が急に変わらないようにするために経過時間で回転
+    private Vector3 defaultPosition; // 前後移動用に保存しておくドットの基準位置
+    private int movedCount = 0; // ランダムドットの方向変えるためのカウンタ
+    private List<float> translateValues = new List<float>(); // 各ドットの平行移動幅を格納
+    private List<float> rotateValues = new List<float>(); // 各ドットの回転量を格納
     void Start()
     {
         //ドット生成用コンポーネントが見つからないなら探す
@@ -33,49 +53,151 @@ public class SwingMotion : MonoBehaviour
         }
 
         //ドットの生成
-        if (type == SWING_TYPE.FORWARDBACK)//前後なら円筒型にする
+        
+        if (direction == SWING_TYPE.FORWARDBACK)//前後なら円筒型にする
         {
             generator.RandomDotsAngle = 180;
-            generator.CreateRandomDot(false);
+            dot = generator.CreateRandomDot(false);
         }
         else
         {
-            generator.CreateRandomDot();
+            dot = generator.CreateRandomDot();
         }
 
-        //ランダムドット群の取得
-        dot = GameObject.FindWithTag("Dots"); // タグ'Dots'を持つオブジェクトを検索
-        if (dot == null)
-        {
-            Debug.LogError("No GameObject with tag 'Dots' found!"); // オブジェクトが見つからない場合のエラー
-        }
         defaultPosition = dot.transform.position;
     }
 
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.Return)) // スイングのオン/オフ
         {
-            isSwinging = !isSwinging;
+            isMoving = !isMoving;
         }
 
-        if (isSwinging && dot != null) // スイングがアクティブで、ドットオブジェクトが存在する場合
+        if (Input.GetKeyDown(KeyCode.R)) // ドットの再生成
         {
-            passedTime += Time.deltaTime;
-            angle = swingAngle * Mathf.Sin(passedTime * frequency * 2 * Mathf.PI); // 角度を計算
-            if (type == SWING_TYPE.SIDE)
+            if (direction == SWING_TYPE.FORWARDBACK)//前後なら円筒型にする
             {
-                dot.transform.localEulerAngles = new Vector3(0, angle, 0);
+                generator.RandomDotsAngle = 180;
+                dot = generator.CreateRandomDot(false);
             }
-            else if (type == SWING_TYPE.UPDOWN)
+            else
             {
-                dot.transform.localEulerAngles = new Vector3(angle, 0, 90);
+                dot = generator.CreateRandomDot();
             }
-            else if (type == SWING_TYPE.FORWARDBACK)
+            if (dot == null)
             {
-                dot.transform.localEulerAngles = new Vector3(90, 0, 0);
-                var offset = swingRange * Mathf.Sin(passedTime * frequency * 2 * Mathf.PI); // 角度を計算
-                dot.transform.position = defaultPosition + new Vector3(0, 0, offset);   
+                Debug.LogError("No GameObject with tag 'Dots' found!"); // オブジェクトが見つからない場合のエラー
+            }
+        }
+
+        //ランダムドット全体を回転させておく
+        if (direction == SWING_TYPE.SIDE)
+        {
+            dot.transform.localEulerAngles = new Vector3(0, 0, 0);
+        }
+        else if (direction == SWING_TYPE.UPDOWN)
+        {
+            dot.transform.localEulerAngles = new Vector3(0, 0, 90);
+        }
+        else if (direction == SWING_TYPE.FORWARDBACK)
+        {
+            dot.transform.localEulerAngles = new Vector3(90, 0, 0);
+        }
+
+
+        if (isMoving && dot != null) // スイングがアクティブで、ドットオブジェクトが存在する場合
+        {
+            if (moveType == MOVE_TYPE.SWING)
+            {
+                passedTime += Time.deltaTime;
+                angle = swingAngle * Mathf.Sin(passedTime * frequency * 2 * Mathf.PI); // 角度を計算
+                if (direction == SWING_TYPE.SIDE)
+                {
+                    dot.transform.localEulerAngles = new Vector3(0, angle, 0);
+                }
+                else if (direction == SWING_TYPE.UPDOWN)
+                {
+                    dot.transform.localEulerAngles = new Vector3(angle, 0, 90);
+                }
+                else if (direction == SWING_TYPE.FORWARDBACK)
+                {
+                    dot.transform.localEulerAngles = new Vector3(90, 0, 0);
+                    var offset = swingRange * Mathf.Sin(passedTime * frequency * 2 * Mathf.PI); // 角度を計算
+                    dot.transform.position = defaultPosition + new Vector3(0, 0, offset);
+                }
+            }
+            if (moveType == MOVE_TYPE.RANDOM)
+            {
+                if (direction == SWING_TYPE.SIDE || movedCount >= 0)
+                {
+                    // 動かす方向の決定
+                    if (translateValues.Count == 0 || movedCount >= changeDirectionFrequency)
+                    {
+                        movedCount = 0;
+                        translateValues.Clear();
+                        rotateValues.Clear();
+
+                        for (int i = 0; i < generator.RandomDotsAmount; i++)
+                        {
+                            var val = Random.Range(-translateRange, translateRange);
+                            var rot = Random.Range(-rotateRange, rotateRange);
+                            translateValues.Add(val);
+                            rotateValues.Add(rot);
+                        }
+                    }
+
+                    //全ドットを動かす
+                    int j = 0;
+                    foreach (Transform child in dot.transform)
+                    {
+                        var grandChild = child.GetChild(0);
+                        //上下に動かす
+                        {
+                            var maxHeight = generator.RandomDotsMaxHeight;
+                            var nextY = grandChild.transform.localPosition.y + Time.deltaTime * translateValues[j];
+                            if (nextY > maxHeight)
+                            {
+                                translateValues[j] *= -1;
+                                nextY = maxHeight * 2 - nextY;
+                            }
+                            else if (nextY < -maxHeight)
+                            {
+                                translateValues[j] *= -1;
+                                nextY = -maxHeight * 2 - nextY;
+                            }
+                            grandChild.localPosition = new Vector3(0, nextY, generator.RandomDotsDistance);
+                        }
+                        //左右に回す
+                        {
+                            var maxY = generator.RandomDotsAngle;
+                            var nextY = child.localEulerAngles.y + Time.deltaTime * rotateValues[j];
+                            if (nextY > 180) nextY -= 360;
+                            if (nextY > maxY)
+                            {
+                                rotateValues[j] *= -1;
+                                nextY = maxY * 2 - nextY;
+                            }
+                            else if (nextY < -maxY)
+                            {
+                                rotateValues[j] *= -1;
+                                nextY = -maxY * 2 - nextY;
+                            }
+                            child.localEulerAngles = new Vector3(0, nextY, 0);
+                        }
+
+                        //サイズ調整
+                        {
+                            var distance = Vector3.Distance(grandChild.transform.position, generator.viewCamera.transform.position); // カメラからの距離に基づいたサイズ調整
+                            float adjustedSize = generator.RandomDotsSize * distance / generator.RandomDotsDistance;
+                            grandChild.transform.localScale = new Vector3(adjustedSize, adjustedSize, adjustedSize);
+                        }
+
+                        j++;
+                    }
+                }
+                movedCount++;
             }
         }
     }
